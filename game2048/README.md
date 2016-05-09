d0e80d894d0d1edd1771baf4be3d40542a400e5a
emloxe <qwrtyp@live.cn>
2016-05-09 17:16:54 +0800

    add game2048

---
+++ game2048/README.md
@@ -0,0 +1,8 @@
+2048小游戏
+===========
+演示地址：
+https://emloxe.github.io/small_demo/game2048/index.html
+
+说明
+------
+根据慕课网 liuyubobobo 老师的课程 [慕课网2048私人订制](http://www.imooc.com/learn/76)所做出来的
---
+++ game2048/js/showanimation2048.js
@@ -0,0 +1,35 @@
+function showNumberWithAnimation(i,j,number){
+	var numberCell = $('#number-cell-'+i+'-'+j);
+	numberCell.css({
+		"background-color" : getNumberBackgroundColor( board[i][j] ) ,
+		"color"  : getNumberColor( board[i][j] )
+	});
+	numberCell.text(board[i][j]);
+	numberCell.animate({
+		width : cellSideLength,
+		height: cellSideLength,
+		top    : getPosTop(i,j),
+		left   : getPosLeft(i,j),
+	},50);
+}
+
+
+
+function showMoveAnimation(startI,startJ,endI,endJ,speed){
+	var numberCellStart = $('#number-cell-'+startI+'-'+startJ);
+	numberCellStart.animate({
+		top    : getPosTop(endI,endJ),
+		left   : getPosLeft(endI,endJ)
+	},speed);
+}
+
+
+
+
+
+
+
+
+
+
+
